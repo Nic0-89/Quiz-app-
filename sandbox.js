f7ee32d09080f4b1809bd79cf589bb7466355b49
Nicole Avila <avila.nicky89@gmail.com>
2023-12-16 14:50:04 +0100

    html, css and basic Js done

---
+++ sandbox.js
@@ -0,0 +1,98 @@
+const topicOne = [
+    {
+        question: 'Here goes the first trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: true },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the second trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: true },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the third trivia question?',
+        answers: [
+            { text: 'answer 1', correct: true },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the fourth trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: true },
+        ]
+    }
+]
+
+const topicTwo = [
+    {
+        question: 'Here goes the first trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: true },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the second trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: true },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the third trivia question?',
+        answers: [
+            { text: 'answer 1', correct: true },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: false },
+        ]
+    },
+    {
+        question: 'Here goes the fourth trivia question?',
+        answers: [
+            { text: 'answer 1', correct: false },
+            { text: 'answer 2', correct: false },
+            { text: 'answer 3', correct: false },
+            { text: 'answer 4', correct: true },
+        ]
+    }
+]
+let questionAsked = document.getElementById('question');
+let answerChose = document.getElementById('answer-buttons');
+let nextButton = document.getElementsByClassName('btn-next');
+
+
+function startQuestion() {
+    Pass
+}
+
+function displayQuestion() {
+    Pass
+}
+
+function resetQuiz() {
+    Pass
+}
+
+function answerSelected(){
+    pass
+}
+
